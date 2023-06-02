47da6f2b8f45bfb563b2a8a8980a769253bd4d78
Murilo Alves Zapiello <54750759805@info.local>
2023-06-02 17:14:43 -0300

    é isso ai

--- projeto_gamer_mvc/Controllers/JogadorController.cs
+++ projeto_gamer_mvc/Controllers/JogadorController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using projeto_gamer_mvc.infra;
+using projeto_gamer_mvc.Models;
 
 namespace projeto_gamer_mvc.Controllers
 {
@@ -21,6 +22,18 @@ namespace projeto_gamer_mvc.Controllers
             ViewBag.Jogador = c.Jogador.ToList();
             ViewBag.Equipe = c.Equipe.ToList();
             return View();
+        }
+
+        [Route("Cadastrar")]
+
+        public IActionResult Cadastrar(IFormCollection form)
+        {
+            Jogador novoJogador = new Jogador();
+
+            novoJogador.IdJogador = int.Parse(form["Id Jogador"].ToString());
+            
+
+            return LocalRedirect("~/Jogador/Listar");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
